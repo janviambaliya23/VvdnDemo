174a5cbbafc2371a8f2d7330be3ad1ece5d92420
jambaliya <janvi.ambaliya@vvdntech.in>
2026-03-13 15:01:22 +0530

    remove line

--- server.c
+++ server.c
@@ -4,5 +4,4 @@
 >>>>>>> d11b9d5 (add sentance like work in git)
 printf("only for git practice in sever.c\n ");
 printf("again i am modifying.\n");
-printf("checking diff command\n");
 printf("again i am modifying.\n");
