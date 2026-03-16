2c6ebe9837129b1e492a1287d287bba0de9f2762
jambaliya <janvi.ambaliya@vvdntech.in>
2026-03-16 10:22:30 +0530

    add sentance like work in git

--- server.c
+++ server.c
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 #include<stdio.h>
+=======
+>>>>>>> d11b9d5 (add sentance like work in git)
 printf("only for git practice in sever.c\n ");
 printf("again i am modifying.\n");
 printf("again i am modifying.\n");
